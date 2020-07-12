24f3851d201d71e912e3d8fa6421598cde33f36a
Jani Mikkonen <jani.mikkonen@gmail.com>
2020-07-12 19:40:49 +0300

    from template

--- build.py
+++ build.py
@@ -1,12 +1,24 @@
 import feedparser
+import jinja2
+from pathlib import Path
+from pprint import pprint
 
-URL="https://rasjani.github.io/feed.xml"
-feed = feedparser.parse(URL)
-print("### Latests posts")
-for post in feed["entries"][0:5]:
-    title = post["title"]
-    url = post["links"][0]["href"]
-    ts = post["published_parsed"]
-    date = f"* {ts.tm_mday:02}.{ts.tm_mon:02}.{ts.tm_year}"
-    print(f"{date} - [{title}]({url})")
-print("\nMore in the [blog](http://rasjani.github.io)")
+BLOGURL="https://rasjani.github.io"
+FEED=f"{BLOGURL}/feed.xml"
+MAX_POSTS=5
+template = None
+
+
+def format_date(ts):
+    return f"{ts.tm_mday:02}.{ts.tm_mon:02}.{ts.tm_year}"
+
+with Path("README.template").open("r") as f:
+    template = jinja2.Template(f.read())
+
+feed = feedparser.parse(FEED)
+posts = list(map(lambda post: {"date": format_date(post["published_parsed"]),"title": post["title"], "link": post["links"][0]["href"]}, feed["entries"][0:MAX_POSTS]))
+
+
+with Path("README.md").open("w") as f:
+    f.write(template.render(blogurl=BLOGURL, posts=posts))
+
