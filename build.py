0a8769e5cd73c195481735ae6bf61f1a065eed44
Jani Mikkonen <jani.mikkonen@gmail.com>
2020-07-12 16:54:39 +0300

    generate readme code

---
+++ build.py
@@ -0,0 +1,11 @@
+import feedparser
+
+URL="https://rasjani.github.io/feed.xml"
+feed = feedparser.parse(URL)
+print("### Latests posts")
+for post in feed["entries"][0:5]:
+    title = post["title"]
+    url = post["links"][0]["href"]
+    ts = post["published_parsed"]
+    date = f"{ts.tm_mday:02}.{ts.tm_mon:02}.{ts.tm_year}"
+    print(f"{date} - [{title}]({url})")
