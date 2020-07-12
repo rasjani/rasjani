56abfd76b15624265de4e4c9d67730a13a45e91b
Jani Mikkonen <jani.mikkonen@gmail.com>
2020-07-12 19:08:51 +0300

    link

--- build.py
+++ build.py
@@ -9,3 +9,4 @@ for post in feed["entries"][0:5]:
     ts = post["published_parsed"]
     date = f"* {ts.tm_mday:02}.{ts.tm_mon:02}.{ts.tm_year}"
     print(f"{date} - [{title}]({url})")
+print("\nMore in the [blog](http://rasjani.github.io)")
